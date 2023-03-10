00e5fa40651c3f87895ff966f86f0cf4172a109b
doziechuks <doziechuks1010@gmail.com>
2023-03-10 13:41:22 +0100

    created a missionVision component and styled accordingly

--- src/components/coreValues/corevalues.component.jsx
+++ src/components/coreValues/corevalues.component.jsx
@@ -4,6 +4,7 @@ import classes from './coreValues.module.css';
 const CoreValues = () => {
 
   const [showScroll, setShowScroll] = useState(false);
+  const [showMobileScroll, setShowMobileScroll] = useState(false);
 
   const handleScroll = () => {
     if(window.scrollY >= 500){
@@ -11,11 +12,20 @@ const CoreValues = () => {
     }else{
       setShowScroll(false);
     }
+    if (window.scrollY >= 1200) {
+      setShowMobileScroll(true);
+    } else {
+      setShowMobileScroll(false);
+    }
   };
   window.addEventListener('scroll', handleScroll);
 
   return (
-    <div className={`${classes.container} ${showScroll && classes.show}`}>
+    <div
+      className={`${classes.container} ${showScroll && classes.show} ${
+        showMobileScroll && classes.mobileShow
+      }`}
+    >
       <h2>our core values</h2>
       <ul className={classes.values}>
         <li>Lorem amet, consectetur adipisicing elit.</li>
--- src/components/missionVision/missionVision.component.jsx
+++ src/components/missionVision/missionVision.component.jsx
@@ -2,16 +2,27 @@ import classes from './missionVision.module.css';
 
 import Title from '../titleComponent/title.component';
 import CoreValues from '../coreValues/corevalues.component';
+import MissionVisionContainer from '../mission/mission.component';
 
  const MissionVision = () => {
-  return ( 
+  return (
     <section className={classes.wrapper}>
-      <Title heading='our' title='mission and vision' isAboutPage />
+      <Title heading="our" title="mission and vision" isAboutPage />
       <div className={classes.container}>
         <CoreValues />
+        <MissionVisionContainer
+          title="our mission"
+          mission="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam, quo officia sed fugit amet veniam iste eligendi odio voluptate"
+          isMission
+        />
+        <MissionVisionContainer
+          title="our vision"
+          mission="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsam, quo officia sed fugit amet veniam iste eligendi odio"
+          isVision
+        />
       </div>
     </section>
-   );
+  );
 }
  
 export default MissionVision;
