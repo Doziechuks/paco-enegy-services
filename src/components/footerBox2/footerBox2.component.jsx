e95e551b4c97ab853d825694a37b7b872046781f
doziechuks <doziechuks1010@gmail.com>
2023-03-13 22:37:43 +0100

    added a smooth scroll to put function on the footer

--- src/components/footerBox2/footerBox2.component.jsx
+++ src/components/footerBox2/footerBox2.component.jsx
@@ -1,33 +1,41 @@
 import classes from './footerBox2.module.css';
+
 import { useHistory } from 'react-router-dom';
 
 const FooterBox2 = () => {
   const history = useHistory();
+
+   const handleScrollTop = () => {
+     window.scrollTo({
+       top: 0,
+       behavior: "smooth",
+     });
+   };
   return (
     <div className={classes.box2}>
       <h2>quick link</h2>
       <ul>
-        <li onClick={() => history.push("/")}>home</li>
+        <li onClick={() => {history.push("/"); handleScrollTop();}}>home</li>
         <hr />
-        <li onClick={() => history.push("/about")}>about</li>
+        <li onClick={() => {history.push("/about"); handleScrollTop();}}>about</li>
         <hr />
-        <li onClick={() => history.push("/services/solarinstallations")}>
+        <li onClick={() => {history.push("/services/solarinstallations"); handleScrollTop();}}>
           solar installation and maintenance
         </li>
         <hr />
-        <li onClick={() => history.push("/services/CCTVinstallations")}>
+        <li onClick={() => {history.push("/services/CCTVinstallations"); handleScrollTop();}}>
           CCTV installation
         </li>
         <hr />
-        <li onClick={() => history.push("/services/generalhousewiring")}>
+        <li onClick={() => {history.push("/services/generalhousewiring"); handleScrollTop();}}>
           general house wiring
         </li>
         <hr />
-        <li onClick={() => history.push("/services/partnership")}>
+        <li onClick={() => {history.push("/services/partnership"); handleScrollTop();}}>
           partnership
         </li>
         <hr />
-        <li onClick={() => history.push("/contactus")}>contact us</li>
+        <li onClick={() => {history.push("/contactus"); handleScrollTop();}}>contact us</li>
       </ul>
     </div>
   );
