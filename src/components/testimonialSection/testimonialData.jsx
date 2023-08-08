f6032d95c73ec1c1237aad05d88a51d72fdff438
doziechuks <doziechuks1010@gmail.com>
2023-08-08 22:43:55 +0100

    updated the services and testimonial

--- src/components/testimonialSection/testimonialData.jsx
+++ src/components/testimonialSection/testimonialData.jsx
@@ -6,34 +6,34 @@ const testimonialData = [
     name: "maria ferguson",
     title: "office manager",
     quote:
-      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+      "PACO energy services transformed our home with solar power. Now, we enjoy savings and a cleaner planet. Highly recommended!",
   },
   {
     id: 2,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     name: "john doe",
-    title: "regular guy",
+    title: "Frontend developer",
     quote:
-      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+      "Impressed by PACO energy service's innovation. Our business is greener and more efficient, thanks to their expert solar installation.",
   },
   {
     id: 3,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
     name: "peter smith",
-    title: "product designer",
+    title: "House keeper",
     quote:
-      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+      "Choosing PACO energy services was a game-changer. Their support and top-notch solar system exceeded my expectations. Thrilled with the results!",
   },
   {
     id: 4,
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     name: "susan andersen",
-    title: "the boss",
+    title: "HR USKA solutions",
     quote:
-      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+      "Switching to solar with PACO energy services was the best decision. Our energy bills dropped, and we're proud to contribute to a sustainable future.",
   },
 ];
 
--- src/pages/servicesPage/servicesPageData.js
+++ src/pages/servicesPage/servicesPageData.js
@@ -1,5 +1,5 @@
-import electrical from '../../assets/electricalWiring.jpg';
-import solar from '../../assets/solarField.avif';
+import electrical from "../../assets/electricalWiring.jpg";
+import solar from "../../assets/solarField.avif";
 import cctv from "../../assets/cctvFix.jpg";
 import partnership from "../../assets/partnership.avif";
 import cctvAd from "../../assets/cctvad.png";
@@ -8,7 +8,7 @@ import solar1 from "../../assets/solar.png";
 import solar2 from "../../assets/solar1.png";
 import solarBattery from "../../assets/solarBattery.png";
 import charger from "../../assets/solarcharger.png";
-import solarr from '../../assets/solarr.png';
+import solarr from "../../assets/solarr.png";
 import solarTraining from "../../assets/solarTraining.png";
 import training from "../../assets/training.png";
 import cctvTraining from "../../assets/solartrainingAd.png";
@@ -18,38 +18,31 @@ export const servicesPageData = {
     id: 1,
     title: "solar installation and maintenance",
     routeName: "solarinstallations",
-    backgroundImage:`${solar}`,
-    info: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-            molestiae sunt. Sapiente at maxime voluptate, itaque optio aperiam.
-            Consequuntur, repudiandae dolorem distinctio ex eveniet debitis
-            recusandae, asperiores quae consectetur sint aspernatur minus
-            obcaecati praesentium dolores! Veniam, explicabo vel atque
-            asperiores aspernatur in molestiae, ratione laudantium recusandae
-            iusto excepturi odio, voluptatem commodi placeat adipisci. Deleniti
-            repudiandae pariatur dicta tempora facere voluptatum itaque
-            distinctio impedit non obcaecati dolore, accusantium quidem quasi`,
+    backgroundImage: `${solar}`,
+    info: ` Our comprehensive solar services encompass tailored solutions for residences, businesses, and industries, ensuring energy efficiency, cost savings, and environmental benefits. From initial consultation to seamless installation, our experts guide clients through the solar journey. Moreover, our dedicated maintenance ensures long-term system performance, including routine checkups, real-time monitoring, and responsive support, guaranteeing continued efficiency and peace of mind.`,
     services: [
       "solar installation",
       "sales of solar material/gadgets",
       "solar maintenance",
       "solar installaion trainings",
     ],
-    photoGallery: [solarr,floodLight, charger, solar1, training, solar2, solarTraining, solarBattery],
+    photoGallery: [
+      solarr,
+      floodLight,
+      charger,
+      solar1,
+      training,
+      solar2,
+      solarTraining,
+      solarBattery,
+    ],
   },
   CCTVinstallations: {
     id: 2,
     title: "CCTV camera  installation",
-    backgroundImage:`${cctv}`,
+    backgroundImage: `${cctv}`,
     routeName: "CCTVinstallations",
-    info: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-            molestiae sunt. Sapiente at maxime voluptate, itaque optio aperiam.
-            Consequuntur, repudiandae dolorem distinctio ex eveniet debitis
-            recusandae, asperiores quae consectetur sint aspernatur minus
-            obcaecati praesentium dolores! Veniam, explicabo vel atque
-            asperiores aspernatur in molestiae, ratione laudantium recusandae
-            iusto excepturi odio, voluptatem commodi placeat adipisci. Deleniti
-            repudiandae pariatur dicta tempora facere voluptatum itaque
-            distinctio impedit non obcaecati dolore, accusantium quidem quasi`,
+    info: ` Our CCTV camera installation service offers enhanced security for homes and businesses. We provide expert consultation, strategically positioning high-quality cameras to cover critical areas. Our professionals ensure seamless installation, integrating cutting-edge technology for real-time monitoring. With a focus on deterring potential threats and safeguarding assets, our CCTV solutions offer peace of mind and proactive security management.`,
     services: [
       "CCTV camera installation",
       "sales of CCTV cameras",
@@ -57,7 +50,9 @@ export const servicesPageData = {
       "CCTV camera installaion trainings",
     ],
     photoGallery: [
-      "https://i.ibb.co/ZYW3VTp/brown-brim.png",cctvAd, cctvTraining,
+      "https://i.ibb.co/ZYW3VTp/brown-brim.png",
+      cctvAd,
+      cctvTraining,
       "https://i.ibb.co/QdJwgmp/brown-cowboy.png",
       "https://i.ibb.co/RjBLWxB/grey-brim.png",
     ],
@@ -65,17 +60,9 @@ export const servicesPageData = {
   generalhousewiring: {
     id: 3,
     title: "general house wiring",
-    backgroundImage:`${electrical}`,
+    backgroundImage: `${electrical}`,
     routeName: "generalhousewiring",
-    info: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-            molestiae sunt. Sapiente at maxime voluptate, itaque optio aperiam.
-            Consequuntur, repudiandae dolorem distinctio ex eveniet debitis
-            recusandae, asperiores quae consectetur sint aspernatur minus
-            obcaecati praesentium dolores! Veniam, explicabo vel atque
-            asperiores aspernatur in molestiae, ratione laudantium recusandae
-            iusto excepturi odio, voluptatem commodi placeat adipisci. Deleniti
-            repudiandae pariatur dicta tempora facere voluptatum itaque
-            distinctio impedit non obcaecati dolore, accusantium quidem quasi`,
+    info: ` Our comprehensive house wiring service covers both surface and conduit installations, tailored to your preferences. For a clean and modern look, our surface wiring solutions ensure seamless integration of electrical systems without invasive modifications. Alternatively, our conduit wiring offers enhanced protection, concealing wires within durable conduits for safety and aesthetics. With our expertise, your home will boast efficient, safe, and visually appealing electrical installations that meet your unique needs.`,
     services: [
       "general house wiring",
       "electrical maintenance",
@@ -91,17 +78,9 @@ export const servicesPageData = {
   partnership: {
     id: 4,
     title: "partnership",
-    backgroundImage:`${partnership}`,
+    backgroundImage: `${partnership}`,
     routeName: "partnership",
-    info: ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo,
-            molestiae sunt. Sapiente at maxime voluptate, itaque optio aperiam.
-            Consequuntur, repudiandae dolorem distinctio ex eveniet debitis
-            recusandae, asperiores quae consectetur sint aspernatur minus
-            obcaecati praesentium dolores! Veniam, explicabo vel atque
-            asperiores aspernatur in molestiae, ratione laudantium recusandae
-            iusto excepturi odio, voluptatem commodi placeat adipisci. Deleniti
-            repudiandae pariatur dicta tempora facere voluptatum itaque
-            distinctio impedit non obcaecati dolore, accusantium quidem quasi`,
+    info: ` Collaborating with us means joining forces for mutual growth. We value partnerships that harness our expertise and resources to achieve shared goals. Whether you're a business seeking innovative solutions or an organization aiming to make a difference, our partnership approach emphasizes open communication, resource synergy, and collective success. Together, we unlock opportunities, drive innovation, and create lasting positive impacts.`,
     services: ["general partnership", "assistance and aids"],
     photoGallery: [
       "https://i.ibb.co/ZYW3VTp/brown-brim.png",
